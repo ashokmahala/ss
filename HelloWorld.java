82afdcc9c8e3cac8b3630a05b682da1c9c23ef69
Ashok Mahala <112241384+ashokmahala@users.noreply.github.com>
2023-01-28 13:40:02 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World! my name is ashok mahala welocome to msrit"); 
+        System.out.println("Hello, World! my name is ashok mahala welocome to msrit mca"); 
     }
 }
